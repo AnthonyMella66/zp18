5158268a4f7221d4c6b20542313a193e7e5c38d6
anthony mella <AnthonyXavier@protonmail.com>
2020-04-19 22:00:51 -0400

    submit

---
+++ Unity/simulation_one/Assets/State.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+/*
+* Tyler Philips
+*
+*State objects will have a timer that can be controlled with StartState and StopState functions
+*this time will be used to keep track of user progression
+*/
+public class State : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    //void Update () {
+    //}
+
+    //global variables
+    private Stopwatch timer;
+    private float id;
+    private string output;
+
+    public State(float identity)
+    {
+        this.timer = new Stopwatch();
+        this.id = identity;
+        this.output = "";
+    }
+
+    public void StartState()
+    {
+        this.timer.Start();
+    }
+
+    public void EndState()
+    {
+        this.timer.Stop();
+        this.output = "Time elapsed in State " + this.id + " = " + this.timer.Elapsed;
+    }
+
+    public string GetOutput()
+    {
+        return this.output;
+    }
+
+    //writing to an output file
+    //string[] stateStats = { "Score Processing\n=================\n~Time measured through each game state~", state0.GetOutput(), state1.GetOutput(), state2.GetOutput(), state3.GetOutput() };
+    //System.IO.File.WriteAllLines(@"C:\Users\Public\TestFolder\WriteLines.txt", stateStats);
+
+
+}
---
+++ Unity/simulation_one/Assets/StateList.cs
@@ -0,0 +1,58 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+/*
+* Tyler Philips
+*
+*This class is used to create a list of State objects as found in State.cs
+* in the initialization of a StateList, the number of states (n) that will exist needs to be taken as a parameter
+* the StateList will then create `n`State objects for the user.
+*/
+public class StateList : MonoBehaviour
+{
+
+    //global variables
+    private List<State> list = new List<State>();
+    private count;
+
+
+    public StateList(float n)
+    {
+
+        var temp = new List<State>();
+
+        //filling list with n State variables
+        for (int i = 0; i < n; i++)
+        {
+            temp.Add(new State(i));
+        }
+
+        //initializing variables
+        this.count = n;
+        this.list = temp;
+    }
+
+
+    public float Average()
+    {
+        float average = 0;
+
+
+        //calculate
+       // for (int i = 0; i < this.count; i++)
+        //{
+            
+        //}
+
+        return average;
+
+    }
+
+}
+
+
+
+
+
